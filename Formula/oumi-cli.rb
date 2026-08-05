9cc720f2aba1594b815ce401bf225d97dc4e3a1e
Andy LeGrand <andy.legrand@oumi.ai>
2026-08-05 13:00:10 -0700

    Update version 085 fix sha256 (#2)
    * updated version
    
    * Fix sha256
    
    ---------
    
    Co-authored-by: Andy LeGrand <andy@oumi.ai>

--- Formula/oumi-cli.rb
+++ Formula/oumi-cli.rb
@@ -6,18 +6,18 @@ class OumiCli < Formula
   on_macos do
     on_arm do
       url "https://oumi-public-downloads.s3.us-west-2.amazonaws.com/cli/#{version}/oumi-cli-macos-arm64.tar.gz"
-      sha256 "581863109b1ba49af3aedfad3190768d843224c812be658717a08159c426013d"
+      sha256 "ec5342a80e6abd119ed67784abc4165ddb05c46c70218686df8ee5873125e875"
     end
   end
 
   on_linux do
     on_intel do
       url "https://oumi-public-downloads.s3.us-west-2.amazonaws.com/cli/#{version}/oumi-cli-linux-x86_64.tar.gz"
-      sha256 "694c414eccab4b0ef475f6ecdb718b5f33c9e82491ce5b7025f2fed24965f3df"
+      sha256 "9ca7b86cd8484dd8602cca912ce162e8337c7206cb5ab4932dcfb7ed5d5d0e62"
     end
     on_arm do
       url "https://oumi-public-downloads.s3.us-west-2.amazonaws.com/cli/#{version}/oumi-cli-linux-arm64.tar.gz"
-      sha256 "e02b7e6f10f0c8c847f7cf6c7d69816b084da441adc0ba2e439a9be5e1b0dca3"
+      sha256 "df45adb8818197df517e1e31d79a25e69f70fd1a53af3134c35b75e2a017f645"
     end
   end
 
